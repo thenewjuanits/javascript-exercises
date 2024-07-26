ebcfa901a69c5e1d66f15db0f879eec7752db75e
thenewjuanits <ramosjuanito051@gmail.com>
2024-07-26 14:36:32 +0800

    Solved 01_helloWorld and 02_repeatString

---
+++ 02_repeatString/trial.js
@@ -0,0 +1,11 @@
+function repeatTheString(str, num) {
+    let result = ""
+    
+
+    for (let i = 0; i <= num; i++) {
+        result +=str;
+    }
+    console.log(result)
+}
+
+repeatTheString("juanito", 5)
